dfeee83fcabae8738acc7db1eade56c2d84d5ad7
Sekitha <sekitha.senanayake@gmail.com>
2026-01-07 09:48:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,23 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+#function to connect to database
 def get_db_connection():
     conn = sqlite3.connect('songs.db')
-    conn.row_factory = sqlite3.Row # Allows name-based access to columns
+    conn.row_factory = sqlite3.Row 
     return conn
 
+#function to get songs according to each metric
+def get_mood_songs(energy, valence):
+    with get_db_connection() as db:
+        return db.execute("""SELECT track_id, track_name, track_artist, track_popularity
+            FROM songs WHERE valence BETWEEN ? AND ?
+            AND energy BETWEEN ? AND ?
+            ORDER BY RANDOM()
+            LIMIT 30""",
+            (valence[0], valence[1], energy[0], energy[1])
+        ).fetchall()
+    
 # default route, loads home page
 @app.route("/")
 def index():
@@ -22,37 +34,28 @@ def index():
 # route when user mood is happy
 @app.route("/happy")
 def happy():
-    db = get_db_connection()
-    happySongs = db.execute("SELECT track_id, track_name, track_artist, track_popularity FROM songs WHERE ((valence >= 0.70 AND valence <= 1.00) AND (energy >= 0.60 AND energy <= 0.90)) ORDER BY RANDOM() LIMIT 30").fetchall()
-    db.close()
+    happySongs = get_mood_songs((0.60, 0.90), (0.70, 1.00))
     happy = "happy"
     return render_template("mood.html", mood=happySongs, type=happy)
 
 # route when user mood is sad
 @app.route("/sad")
 def sad():
-    db = get_db_connection()
-    sadSongs = db.execute("SELECT track_id, track_name, track_artist, track_popularity FROM songs WHERE ((valence >= 0.00 AND valence <= 0.30) AND (energy >= 0.00 AND energy <= 0.40)) ORDER BY RANDOM() LIMIT 30").fetchall()
-    db.close()
+    sadSongs = get_mood_songs((0.00, 0.40), (0.00, 0.30))
     sad = "sad"
     return render_template("mood.html", mood=sadSongs, type=sad)
 
 # route when user mood is energetic
 @app.route("/energetic")
 def energetic():
-    db = get_db_connection()
-    energeticSongs = db.execute(
-        "SELECT track_id, track_name, track_artist, track_popularity FROM songs WHERE ((valence >= 0.40 AND valence <= 1.00) AND (energy >= 0.80 AND energy <= 0.90)) ORDER BY RANDOM() LIMIT 30").fetchall()
-    db.close()
+    energeticSongs = get_mood_songs((0.80, 0.90), (0.40, 1.00))
     energetic = "energetic"
     return render_template("mood.html", mood=energeticSongs, type=energetic)
 
 # route when user mood is chill
 @app.route("/chill")
 def chill():
-    db = get_db_connection()
-    chillSongs = db.execute("SELECT track_id, track_name, track_artist, track_popularity FROM songs WHERE ((valence >= 0.40 AND valence <= 0.70) AND (energy >= 0.20 AND energy <= 0.50)) ORDER BY RANDOM() LIMIT 30").fetchall()
-    db.close()
+    chillSongs = get_mood_songs((0.20, 0.50), (0.40, 0.70))
     chill = "chill"
     return render_template("mood.html", mood=chillSongs, type=chill)
 
@@ -82,9 +85,10 @@ def favorites():
             session["favorites"] = []
             session.modified = True
             #clears the current favorite songs when user clicks "Clear Favorites"
+            return redirect("/favorites")
 
         db.close()
-        return redirect("/favorites")
+        return "", 204
 
     # if user has no favorites currently, then a list is created
     if len(session["favorites"]) == 0:
