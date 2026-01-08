8d8df4bf0fdba776ed6309450f47f56788bebcdd
Sekitha <sekitha.senanayake@gmail.com>
2026-01-08 12:51:25 +0530

    Update favorites.js

--- static/js/favorites.js
+++ static/js/favorites.js
@@ -1,29 +1,32 @@
 function addToFavorites(songId) 
             {
-                fetch("/favorites", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/x-www-form-urlencoded"
-                    },
-                    body: "song_id=" + encodeURIComponent(songId)
-                })
+                fetch("/favorites", 
+                    {
+                        method: "POST",
+                        headers: 
+                            {
+                                "Content-Type": "application/x-www-form-urlencoded"
+                            },
+                        body: "song_id=" + encodeURIComponent(songId)
+                    })
 
-                .then(response => {
-                if (response.ok) 
-                {
-                    showMessage("⭐ Added to favorites!");
-                }
-                });
+                .then(response => 
+                    {
+                        if (response.ok) 
+                            {
+                                showMessage("⭐ Added to favorites!");
+                            }
+                    });
             }
 
-            function showMessage(text) 
+function showMessage(text) 
             {
-            const msg = document.getElementById("message");
-            msg.textContent = text;
-            msg.style.display = "block";
+                const msg = document.getElementById("message");
+                msg.textContent = text;
+                msg.style.display = "block";
 
-            setTimeout(() => 
-            {
-                msg.style.display = "none";
-            }, 2000);
+                setTimeout(() => 
+                    {
+                        msg.style.display = "none";
+                    }, 2000);
             }
